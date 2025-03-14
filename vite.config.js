59f9450f237661183a6dd5ed61176ae8000fd227
Aaron Bermingham <aaronbrmnghm@gmail.com>
2025-03-14 18:08:37 +0000

    Move index.htnl to root

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 export default {
-    root: 'src',  // Pointing to the src folder where index.html is
+    root: '.',  // Pointing to the src folder where index.html is
     base: process.env.NODE_ENV === 'development' ? '/' : '/data-structures-visualizer/',
     build: {
       outDir: '../dist'  // Output the build files to the root directory's dist folder
