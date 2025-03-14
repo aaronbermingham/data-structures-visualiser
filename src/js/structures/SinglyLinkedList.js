7aa5b2623ae3982187496a3ad228b2c1fb324f45
Aaron Bermingham <aaronbrmnghm@gmail.com>
2025-03-14 17:43:48 +0000

    Add singly linked list structure

---
+++ src/js/structures/SinglyLinkedList.js
@@ -0,0 +1,27 @@
+class Node {
+    constructor(value) {
+        this.val = value;
+        this.next = null;
+    }
+}
+
+class SinglyLinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    push(val) {
+        var node = new Node(val);
+        if (!this.head) {
+            this.head = node;
+            this.tail = this.head;
+        } else {
+            this.tail.next = node;
+            this.tail = node;
+        }
+        this.length++;
+        return this;
+    }
+}
