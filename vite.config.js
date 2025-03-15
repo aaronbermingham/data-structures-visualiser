e6d1d9757ff2eb6082dea3bc5dabea280a65ecf5
Aaron Bermingham <aaronbrmnghm@gmail.com>
2025-03-15 12:14:09 +0000

    Fix typo

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 export default {
     root: '.',  // Pointing to the src folder where index.html is
-    base: process.env.NODE_ENV === 'development' ? '/' : '/data-structures-visualizer/',
+    base: process.env.NODE_ENV === 'development' ? '/' : '/data-structures-visualiser/',
     build: {
       outDir: '../dist'  // Output the build files to the root directory's dist folder
     }
